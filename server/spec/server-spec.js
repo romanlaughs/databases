6cef16b85578ce21a6b3d3c98dbcacf8e6929bbb
Nick Cassano <romanlaughs@gmail.com>
2022-08-10 20:49:31 -0700

    Complete customizing tests

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -8,20 +8,24 @@ const API_URL = 'http://127.0.0.1:3000/classes';
 
 describe('Persistent Node Chat Server', () => {
   const dbConnection = mysql.createConnection({
-    user: 'student',
-    password: 'student',
-    database: 'chat',
+    user: 'root',
+    password: '',
+    database: 'chat'
   });
 
   beforeAll((done) => {
     dbConnection.connect();
 
-       const tablename = ''; // TODO: fill this out
+    const tablename = 'messages'; // TODO: fill this out
+    const users = 'users';
+    const rooms = 'rooms';
 
     /* Empty the db table before all tests so that multiple tests
      * (or repeated runs of the tests)  will not fail when they should be passing
      * or vice versa */
     dbConnection.query(`truncate ${tablename}`, done);
+    // dbConnection.query(`truncate ${users}`, done);
+    // dbConnection.query(`truncate ${rooms}`, done);
   }, 6500);
 
   afterAll(() => {
@@ -29,12 +33,14 @@ describe('Persistent Node Chat Server', () => {
   });
 
   it('Should insert posted messages to the DB', (done) => {
-    const username = 'Valjean';
+    const username = 'user';
     const message = 'In mercy\'s name, three days is all I need.';
-    const roomname = 'Hello';
+    const roomname = 'party room';
     // Create a user on the chat server database.
+
     axios.post(`${API_URL}/users`, { username })
       .then(() => {
+        console.log('AFTER .THEN');
         // Post a message to the node chat server:
         return axios.post(`${API_URL}/messages`, { username, message, roomname });
       })
@@ -65,8 +71,8 @@ describe('Persistent Node Chat Server', () => {
 
   it('Should output all messages from the DB', (done) => {
     // Let's insert a message into the db
-       const queryString = '';
-       const queryArgs = [];
+    const queryString = '';
+    const queryArgs = [];
     /* TODO: The exact query string and query args to use here
      * depend on the schema you design, so I'll leave them up to you. */
     dbConnection.query(queryString, queryArgs, (err) => {
